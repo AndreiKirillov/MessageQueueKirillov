0b566145f566fac2d20710333cf1fcfb3969d3b2
AndreiKirillov <andreikiril1ov@yandex.ru>
2022-09-11 17:22:34 +0300

    ClientCPP

---
+++ MessageQueueKirillov/MyBroker/Server.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "framework.h"
+//#include "Kirillov_lab1_cpp.h"
+//#include "ThreadStorage.h"
+//#include "Connection.h"
+#include <afxsock.h>
+
+// Класс сервера, который будет обрабатывать подключения клиентов
+class Server
+{
+private:
+	CSocket _server;
+
+	//set<unique_ptr<Connection>> _connections;
+	//mutex mtx_for_working_threads;             // синхронизирует доступ к рабочим потокам
+
+
+	void ProcessClient(SOCKET hSock, int client_id);
+	void CloseClient(int client_id);
+public:
+	Server();
+	~Server();
+
+	bool StartUp();
+
+	void WaitForConnection();
+
+	int GetClientsCount();
+};
+

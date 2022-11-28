d29d36c78a46ba6d9dd63fbfaf50abd34435ac2d
AndreiKirillov <andreikiril1ov@yandex.ru>
2022-11-28 12:27:51 +0300

    Sharp client start

---
+++ MessageQueueKirillov/SharpClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SharpClient
+{
+    class Program
+    {
+        static void PrintMenu()
+        {
+            Console.WriteLine("Что сделать дальше?");
+            Console.WriteLine("1 - Написать сообщение юзеру");
+            Console.WriteLine("2 - Написать широковещательное сообщение");
+            Console.WriteLine("3 - Отключиться от сервера");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintMenu();
+        }
+    }
+}

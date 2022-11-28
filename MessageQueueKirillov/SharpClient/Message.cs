517664811333261daa75a6299e5e34c0a0c5a971
AndreiKirillov <andreikiril1ov@yandex.ru>
2022-11-28 20:23:30 +0300

    Class Message

---
+++ MessageQueueKirillov/SharpClient/Message.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SharpClient
+{
+    public enum MessageType
+    { Registration, Exit, Peer2Peer, Broadcast, GetData, Empty, Confirm, Error };
+
+    public enum MessageClient
+    { Broker, User, All };
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct MessageHeader
+    {
+        [MarshalAs(UnmanagedType.I4)]
+        public MessageClient recipient;
+        [MarshalAs(UnmanagedType.I4)]
+        public int recipient_id_size;
+
+        [MarshalAs(UnmanagedType.I4)]
+        public MessageClient sender;
+        [MarshalAs(UnmanagedType.I4)]
+        public int sender_id_size;
+
+        [MarshalAs(UnmanagedType.I4)]
+        public MessageType type;
+        [MarshalAs(UnmanagedType.I4)]
+        public int size;
+    };
+
+    internal class Message
+    {
+        private MessageHeader _header;
+        private string _recipient_id;
+        private string _sender_id;
+        private string _data;
+        static Encoding? cp866 = null;
+
+        public Message()
+        {
+            _header = new MessageHeader();
+            _recipient_id = string.Empty;
+            _sender_id = string.Empty;
+            _data = string.Empty;
+        }
+
+        public Message(ref MessageHeader header, string data)
+        {
+            _header = header;
+            _recipient_id = string.Empty;
+            _sender_id = string.Empty;
+            _data = data;
+        }
+
+        Encoding get866()
+        {
+            if (cp866 is null)
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                cp866 = Encoding.GetEncoding("CP866");
+            }
+            return cp866;
+        }
+
+        static byte[] toBytes(object obj)     // Функция конвертации объекта в набор байтов
+        {
+            int size = Marshal.SizeOf(obj);
+            byte[] buff = new byte[size];
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(obj, ptr, true);
+            Marshal.Copy(ptr, buff, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return buff;
+        }
+
+        // Функция конструирования объекта типа Т из набора байт
+        static T fromBytes<T>(byte[] buff) where T : struct
+        {
+            T data = default(T);
+            int size = Marshal.SizeOf(data);
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.Copy(buff, 0, ptr, size);
+            var d = Marshal.PtrToStructure(ptr, data.GetType());
+            if (d is not null)
+            {
+                data = (T)d;
+            }
+            Marshal.FreeHGlobal(ptr);
+            return data;
+        }
+
+        public static Boolean Send(Socket destination, Message message)    // Функция отправки сообщения через сокет
+        {
+            try
+            {
+                // отправляем заголовок
+                destination.Send(toBytes(message._header), Marshal.SizeOf(message._header), SocketFlags.None);
+
+                if (message._header.type == MessageType.Empty)
+                    return true;
+
+                if (message._header.recipient == MessageClient.User && message._header.recipient_id_size > 0)
+                {
+                    // отправляем имя отправителя
+                    destination.Send(message.get866().GetBytes(message._recipient_id), message._header.recipient_id_size, SocketFlags.None);
+                }
+
+                if (message._header.sender == MessageClient.User && message._header.sender_id_size > 0)
+                {
+                    // отправляем имя получателя
+                    destination.Send(message.get866().GetBytes(message._sender_id), message._header.sender_id_size, SocketFlags.None);
+                }
+
+                if (message._header.size != 0)
+                {
+                    destination.Send(message.get866().GetBytes(message._data), message._data.Length, SocketFlags.None);
+                }
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine("{0} Error code: {1}.", ex.Message, ex.ErrorCode);
+                return false;
+            }
+            return true;
+        }
+
+        public static Message Read(Socket source)    // Функция чтения сообщения из сокета
+        {
+            Message received_message = new Message();
+            MessageHeader header = new MessageHeader();
+            byte[] buff_for_header = new byte[Marshal.SizeOf(header)];
+
+            source.Receive(buff_for_header, Marshal.SizeOf(header), SocketFlags.None);
+            header = fromBytes<MessageHeader>(buff_for_header);
+
+            if (header.type == MessageType.Confirm || header.type == MessageType.Error || header.type == MessageType.Empty)
+            {
+                received_message._header = header;
+                return received_message;
+            }
+
+            if (header.recipient == MessageClient.User)
+            {
+                byte[] recipient_buff = new byte[header.recipient_id_size];
+                source.Receive(recipient_buff, header.recipient_id_size, SocketFlags.None);
+                received_message._recipient_id = received_message.get866().GetString(recipient_buff, 0, header.recipient_id_size);
+            }
+
+            if (header.sender == MessageClient.User)
+            {
+                byte[] sender_buff = new byte[header.sender_id_size];
+                source.Receive(sender_buff, header.sender_id_size, SocketFlags.None);
+                received_message._sender_id = received_message.get866().GetString(sender_buff, 0, header.sender_id_size);
+            }
+
+            received_message._header = header;
+            if (header.size > 0)
+            {
+                byte[] data_buff = new byte[header.size];
+                source.Receive(data_buff, header.size, SocketFlags.None);
+                received_message._data = received_message.get866().GetString(data_buff, 0, header.size);
+            }
+            return received_message;
+        }
+
+        public void setSender(string username)
+	    {
+		    _sender_id = username;
+		    _header.sender = MessageClient.User;
+		    _header.sender_id_size = username.Length;
+	    }
+
+        public void setRecipient(string username)
+        {
+            _recipient_id = username;
+            _header.recipient = MessageClient.User;
+            _header.recipient_id_size = username.Length;
+        }
+    }
+}
--- MessageQueueKirillov/SharpClient/Program.cs
+++ MessageQueueKirillov/SharpClient/Program.cs
@@ -4,16 +4,23 @@ namespace SharpClient
 {
     class Program
     {
+        enum Action
+        {
+            SendDirectMessage = 1, BroadcastMessage = 2, ExitServer = 3
+        }
+
         static void PrintMenu()
         {
             Console.WriteLine("Что сделать дальше?");
-            Console.WriteLine("1 - Написать сообщение юзеру");
-            Console.WriteLine("2 - Написать широковещательное сообщение");
-            Console.WriteLine("3 - Отключиться от сервера");
+            Console.WriteLine($"{(int)Action.SendDirectMessage} - Написать сообщение юзеру");
+            Console.WriteLine($"{(int)Action.BroadcastMessage} - Написать широковещательное сообщение");
+            Console.WriteLine($"{(int)Action.ExitServer} - Отключиться от сервера");
         }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите имя для подключения к серверу");
+            string username = Console.ReadLine();
             PrintMenu();
         }
     }
